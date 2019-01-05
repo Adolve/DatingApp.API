5bb0cb8e6b63dd50ccd731f278145388a3daff5f
Soufiane Belmir <belmir.soufiane@gmail.com>
2019-01-05 17:44:51 -0600

    Initial

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "You must specify password between 6 and 20")]
+        public string Password { get; set; }
+    }
+}
